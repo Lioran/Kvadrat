d1008c9179632d9155fc53d373c64eb50d787e1a
Lioran <wersil@mail.ru>
2016-03-24 14:27:23 +0300

    two

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <math.h>
 int main () {
-	printf ("Hello world!");
+	double D, x1, x2; int a=0, b=0, c=0;
+	printf ("введите коэффициенты \n");
+	scanf ("%d %d %d", a, b, c);
+ 	D=b*b-4*a*c;
+	if (D < 0) printf ("корней нет");
+	if (D == 0) {
+		x1=(-b/2*a);
+		printf ("корень равен %d", x1);
+	}
+	if (D > 0) {
+		x1=((-b+sqrt(D))/2*a);
+		x2=(-b-sqrt(D)/2*a);
+		printf ("корени равны  %d %d", x1, x2);
+	}
 }
