3fdb90b7c06868b97ac61da87f6e37de0be13ea6
Lioran <wersil@mail.ru>
2016-03-25 09:30:53 +0300

    vse

--- main.c
+++ main.c
@@ -2,19 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 int main () {
-	double D, x1, x2; int a=0, b=0, c=0;
-	printf ("введите коэффициенты \n");
-	scanf ("%d %d %d", a, b, c);
- 	D=b*b-4*a*c;
-	if (D < 0) printf ("корней нет");
-	if (D == 0) {
-		x1=(-b/2*a);
-		printf ("корень равен %d", x1);
-	}
-	if (D > 0) {
-		x1=((-b+sqrt(D))/2*a);
-		x2=(-b-sqrt(D)/2*a);
-		printf ("корени равны  %d %d", x1, x2);
-	}
-return 0;
+	double D, x1, x2;
+	int a = 0, b = 0, c = 0;
+		printf ("введите коэффициенты \n");
+		scanf ("%d %d %d", &a, &b, &c);
+ 		D=b*b-4*a*c;
+		if (D < 0) printf ("корней нет");
+		if (D == 0) {
+			x1 = (-b / (2 * a));
+			printf ("корень равен %lf", x1);
+		}
+		if (D > 0) {
+			x1 = ((-b+sqrt(D)) / (2 * a));
+			x2 = ((-b-sqrt(D))/ (2 * a));
+			printf ("корни равны  %lf %lf \n", x1, x2);
+		}
+	return 0;
 }
